60f5aa23a55923832fe3a386418e3ff9e9ba301e
luca lombardo <lucalombardo03@gmail.com>
2020-02-17 18:44:55 +0100

    fine app

--- EsercizioFrasi/MainWindow.xaml.cs
+++ EsercizioFrasi/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
+
 
 namespace EsercizioFrasi
 {
@@ -26,19 +27,23 @@ namespace EsercizioFrasi
         {
             InitializeComponent();
         }
+        
         List<string> frasi = new List<string>();
         int i = 0;
         
         private void Inserisci_Click(object sender, RoutedEventArgs e)
         {
-            frasi[i] = TxtFrasi.Text;
-            i++;
+            string frase = TxtFrasi.Text;
+            frasi.Add(frase);
             TxtFrasi.Clear();
             TxtFrasi.Focus();
+            i++;
             if (i >= 5) 
             {
                 BtnInserisci.IsEnabled = false;
                 BtnStampa.IsEnabled = true;
+                BtnOrdina.IsEnabled = true;
+                BtnPubblica.IsEnabled = true;
             }
         }
 
@@ -46,15 +51,42 @@ namespace EsercizioFrasi
         {
             for (i = 0; i < frasi.Count; i++)
             {
-                LblStringa.Content += $"Posizione {i} : {frasi[i]} \n";
+                TxtStampa.Text += $"Posizione {i} : {frasi[i]} \n";
             }
-            BtnOrdina.IsEnabled = true;
-            BtnPubblica.IsEnabled = true;
+            
         }
 
         private void BtnOrdina_Click(object sender, RoutedEventArgs e)
         {
-            
+            frasi.Sort();
+            TxtOrdinato.Clear();
+            for (int i = 0; i < frasi.Count; i++)
+            {
+                TxtOrdinato.Text += $" Posizione {i} : {frasi[i]}\n ";
+            }
+        }
+
+        private void BtnPubblica_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(file, true, Encoding.UTF8))
+            {
+                for (int i = 0; i < frasi.Count; i++)
+                    sw.WriteLine("Posizione { i} : { frasi[i]} \n");
+                MessageBox.Show("La lista è stata caricata nel file");
+                sw.Flush();
+            }
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(file))
+                File.Delete(file);
+            TxtOrdinato.Text = "";
+            TxtStampa.Text = "";
+            TxtFrasi.Text = "";
+            BtnOrdina.IsEnabled = false;
+            BtnPubblica.IsEnabled = false;
+            BtnStampa.IsEnabled = false;
         }
     }
 }
